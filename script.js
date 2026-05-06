8d9bd897f186c6bb2e0ff9e9c03b8d24a57f5fdf
Mateus Tamaki <mateustamaki43@gmail.com>
2026-05-06 19:07:24 -0300

    Adicionando animações de início

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 const main = document.querySelector('main');
+const introducao = ['Hello', 'こんにちは', 'Hola', 'Bonjour', 'Ciao', 'Nǐ hǎo', 'Olá', false]
 
 function scrollporSecao(prox) {
     const larguraSecao = prox ? window.innerWidth : -window.innerWidth;
     main.scrollBy({ left: larguraSecao, behavior: "smooth"})
 }
+
+const links = document.querySelectorAll('nav a');
+const sections = document.querySelectorAll('section');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const index = Array.from(sections).indexOf(entry.target);
+        links.forEach(link => link.classList.remove('ativo'));
+        links[index].classList.add('ativo');
+    }
+});
+}, { threshold: 0.5 });
+
+const observerShow = new IntersectionObserver((entries) => {
+    entries.forEach(el => {
+        if (el.isIntersecting) {
+            el.target.classList.add('show');
+        }
+        else {
+            el.target.classList.remove('show');
+        }
+    });
+}, { threshold: 0.2 });
+
+
+sections.forEach(s => observer.observe(s));
+document.querySelectorAll('.autoShow').forEach(el => observerShow.observe(el));
 
 // const container = document.querySelector('main');
 window.addEventListener('wheel', e => {
@@ -17,17 +46,14 @@ window.addEventListener('keydown', e => {
     }
 })
 
-const links = document.querySelectorAll('nav a');
-const sections = document.querySelectorAll('section');
-
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-    if (entry.isIntersecting) {
-        const index = Array.from(sections).indexOf(entry.target);
-        links.forEach(link => link.classList.remove('ativo'));
-        links[index].classList.add('ativo');
-    }
-    });
-}, { threshold: 0.5 });
-
-sections.forEach(s => observer.observe(s));
+window.addEventListener('DOMContentLoaded', e => {
+    introducao.forEach((intr, index) => {
+        setTimeout(e => {
+            if(!intr) {
+                document.getElementsByClassName('introducao')[0].classList.add('fade')
+            } else {
+                document.getElementById('ola').innerText = `👋 ${intr}`
+            }
+        }, index * 300)
+    })
+})
